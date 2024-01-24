330fb7b4df2f4bc5d4f2cd844c7a09aede6e317d
Moneesh143 <moneesh.p2001@gmail.com>
2024-01-24 21:16:39 +0530

    Day 9 Task

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("timer").innerHTML = "10";
+  setTimeout(function () {
+    document.getElementById("timer").innerHTML = "9";
+    setTimeout(function () {
+      document.getElementById("timer").innerHTML = "8";
+      setTimeout(function () {
+        document.getElementById("timer").innerHTML = "7";
+        setTimeout(function () {
+          document.getElementById("timer").innerHTML = "6";
+          setTimeout(function () {
+            document.getElementById("timer").innerHTML = "5";
+            setTimeout(function () {
+              document.getElementById("timer").innerHTML = "4";
+              setTimeout(function () {
+                document.getElementById("timer").innerHTML = "3";
+                setTimeout(function () {
+                  document.getElementById("timer").innerHTML = "2";
+                  setTimeout(function () {
+                    document.getElementById("timer").innerHTML = "1";
+                    setTimeout(function () {
+                      document.getElementById("timer").innerHTML =
+                        "Happy Independence Day :)";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+});
